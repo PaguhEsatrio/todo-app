643b9af99800e6784e4b8229ece4d86da2f56e78
Paguh Esatrio <97024224+PaguhEsatrio@users.noreply.github.com>
2024-06-07 10:45:02 +0700

    task 1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,19 @@
 import React, { useState } from 'react';
-import Header from './components/Header';
-import Login from './components/Login';
 import './App.css';
-
-const satu = () => {
-  const [user, setUser] = useState(null);
-
-  const handleLogin = (user) => {
-    setUser(user);
-  };
-
-  const handleLogout = () => {
-    setUser(null);
-  };
-
-  return (
-    <div className="App">
-      <Header />
-      {user ? (
-        <div className="p-4">
-          <h2 className="text-xl">Welcome, {user.username}!</h2>
-          <button onClick={handleLogout} className="bg-red-500 text-white p-2 mt-4">
-            Logout
-          </button>
-        </div>
-      ) : (
-        <Login onLogin={handleLogin} />
-      )}
-    </div>
-  );
-};
-
+import { getTodos } from './database/todo';
+import TodoList from './components/TodoList';
 
 function App() {
 
+  const [todos, _  ] = useState(getTodos());
 
   return (
     <>
       
+      <h3>Todo App</h3>
+      <TodoList todos={todos}/>
     </>
   )
-};
-
+}
 
 export default App;
---
+++ src/database/todo.js
@@ -0,0 +1,23 @@
+const todos =[
+    {
+      id: 1,
+      title: 'Finish React Course',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Have lunch with Parents',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Study React with Mentor',
+      completed: false,
+    },
+  ];
+
+  const getTodos = () => {
+    return todos;
+  }
+
+  export {getTodos};
